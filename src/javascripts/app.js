3a188466c7071603e5331d20ac26dc5eeb68bbd1
Tyler Sloan <tyler.g.sloan@gmail.com>
2015-12-28 22:22:14 -0500

    styling styling

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,19 +1,11 @@
-import './asyncModules'
-import exclaimify from './exclaimify'
+import moment from './moment.min'
 
-const button = document.getElementById('button')
+var aDay = 24 * 60 * 60 * 1000;
 
-const alertAsyncMessage = function() {
-  // CommonJS async syntax webpack magic
-  require.ensure([], function() {
-    const message = require("./asyncMessage")
-    alert(exclaimify(message))
-  })
-}
+var now = moment();
+var sep22 = moment([2016, 9, 22]);
+var diff = sep22.diff(now);
+var diffInDays = Math.floor((diff / aDay) - 30);
 
-console.log(`
-  asset references like this one:
-    images/gulp.png
-  get updated in js too!`)
-
-// button.addEventListener('click', alertAsyncMessage)
+var daySpot = document.getElementById('js-countdown-number');
+daySpot.innerHTML = diffInDays;
